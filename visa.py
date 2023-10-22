d5adc139a67cb73dcac0d38b1fc3b7d3026f7f7d
rodolfo <rcruz@omers.com>
2023-10-22 10:26:58 -0400

    added multiple accounts

--- visa.py
+++ visa.py
@@ -223,7 +223,7 @@ def start_process():
     # Bypass reCAPTCHA
     global counter
     
-    print("Starting time ", datetime.now.strftime("%-I:%M:%S"))
+    print("Starting time ", datetime.now().strftime("%-I:%M:%S"))
     print("mac num ", MAC, counter)
     info_logger(LOG_FILE_NAME, mac)
     USERNAME = accounts[MAC][counter][0]
